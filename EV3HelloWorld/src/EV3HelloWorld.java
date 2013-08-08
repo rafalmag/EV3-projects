1dae35ae25d67edf17ad859da28da50ecc289f12
gloomyandy <gloomyandy@users.sourceforge.net>
2013-08-08 15:49:44 +0200

    Initial commit

---
+++ EV3HelloWorld/src/EV3HelloWorld.java
@@ -0,0 +1,36 @@
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+
+import lejos.nxt.LCD;
+import lejos.nxt.Button;
+import lejos.nxt.Sound;
+import lejos.util.Delay;
+public class EV3HelloWorld
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Running...");
+        final int SW = LCD.SCREEN_WIDTH;
+        final int SH = LCD.SCREEN_HEIGHT;
+        Button.LEDPattern(4);
+        Sound.beepSequenceUp();
+        Graphics g = new Graphics();
+        g.setFont(Font.getLargeFont());
+        g.drawString("leJOS/EV3", SW/2, SH/2, Graphics.BASELINE|Graphics.HCENTER);
+        Button.LEDPattern(3);
+        Delay.msDelay(4000);
+        Button.LEDPattern(5);
+        g.clear();
+        LCD.refresh();
+        Sound.beepSequence();
+        Delay.msDelay(500);
+        Button.LEDPattern(0);
+
+        
+    }
+
+}
