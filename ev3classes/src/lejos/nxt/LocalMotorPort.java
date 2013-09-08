0ba028ed5cdc8103ac1a71852059c5a3679377f8
gloomyandy <gloomyandy@users.sourceforge.net>
2013-09-08 13:35:22 +0200

    Added missed file from previous commit

---
+++ ev3classes/src/lejos/nxt/LocalMotorPort.java
@@ -0,0 +1,165 @@
+package lejos.nxt;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import lejos.internal.io.NativeDevice;
+
+/**
+ * 
+ * Abstraction for an EV3 output port.
+ * 
+ * TODO: Sort out a better way to do this, or least clean up the magic numbers.
+ *
+ */
+public class LocalMotorPort implements MotorPort {
+    static final byte OUTPUT_POWER = (byte)0xa4;
+    static final byte OUTPUT_START = (byte)0xa6;
+    static final byte OUTPUT_STOP = (byte)0xa3;
+    static final byte OUTPUT_CLR_COUNT = (byte)0xb2;
+    protected static NativeDevice tacho;
+    protected static ByteBuffer bbuf;
+    protected static IntBuffer ibuf;
+    protected static NativeDevice pwm;
+    static
+    {
+        initDeviceIO();
+    }
+    protected static LocalMotorPort [] openPorts = new LocalMotorPort[PORTS];    
+    protected int port;
+    protected int curMode = FLOAT+1; // current mode is unknown
+    protected byte[] cmd = new byte[8];
+    
+    /** {@inheritDoc}
+     */    
+    @Override
+    public boolean open(int port)
+    {
+        synchronized (openPorts)
+        {
+            if (openPorts[port] == null)
+            {
+                openPorts[port] = this;
+                this.port = port;
+                return true;
+            }
+            return false;
+        }
+    }
+   
+    /** {@inheritDoc}
+     */    
+    @Override
+    public void close()
+    {
+        if (port == -1)
+            throw new IllegalStateException("Port is not open");
+        synchronized (openPorts)
+        {
+            openPorts[port] = null;
+            port = -1;
+        }
+    }
+    
+    public static MotorPort getInstance(int port)
+    {
+        return openPorts[port];
+    }
+
+
+    /**
+     * Low-level method to control a motor. 
+     * 
+     * @param power power from 0-100
+     * @param mode defined in <code>BasicMotorPort</code>. 1=forward, 2=backward, 3=stop, 4=float.
+     * @see BasicMotorPort#FORWARD
+     * @see BasicMotorPort#BACKWARD
+     * @see BasicMotorPort#FLOAT
+     * @see BasicMotorPort#STOP
+     */
+    public void controlMotor(int power, int mode)
+    {
+        // Convert lejos power and mode to NXT power and mode
+        if (mode >= STOP)
+            power = 0;
+        else if (mode == 2)
+            power = -power;
+        synchronized(cmd)
+        {
+            cmd[0] = OUTPUT_POWER;
+            cmd[1] = (byte)(1 << port);
+            cmd[2] = (byte)power;
+            pwm.write(cmd,  3);
+            if (mode != curMode)
+            {
+                if (mode <= BACKWARD)
+                {
+                    if (curMode >= STOP)
+                    {
+                        // motor not running start it
+                        cmd[0] = OUTPUT_START;
+                        pwm.write(cmd,  2);
+                    }
+                }
+                else
+                {
+                    if (mode == FLOAT)
+                    {
+                        // need to stop the motor
+                        cmd[0] = OUTPUT_STOP;
+                        // Set float
+                        cmd[2] = (byte)0;
+                        pwm.write(cmd, 3);
+                    }
+                    else
+                    {
+                        // STOP leave motor powered up but with zero power
+                        cmd[0] = OUTPUT_START;
+                        pwm.write(cmd,  2);
+                    }
+                }
+                curMode = mode;
+            }
+        }
+    }
+
+
+    /**
+     * returns tachometer count
+     */
+    public  int getTachoCount()
+    {
+        return ibuf.get(port*3 + 2);
+    }
+    
+    /**
+     *resets the tachometer count to 0;
+     */ 
+    public void resetTachoCount()
+    {
+        synchronized(cmd)
+        {
+            cmd[0] = OUTPUT_CLR_COUNT;
+            cmd[1] = (byte)(1 << port);
+            pwm.write(cmd,  2);
+        }
+    }
+    
+    public void setPWMMode(int mode)
+    {
+     }
+    
+    public int getId()
+    {
+        return this.port;
+    }
+    
+    private static void initDeviceIO()
+    {
+        tacho = new NativeDevice("/dev/lms_motor");
+        bbuf = tacho.mmap(96).getByteBuffer(0, 48);
+        ibuf = bbuf.asIntBuffer();
+        pwm = new NativeDevice("/dev/lms_pwm");
+
+    }
+}
