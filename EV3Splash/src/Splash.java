e90c8450ec0b2f356c6a2795e4427cefc0f2b0fe
gloomyandy <gloomyandy@users.sourceforge.net>
2013-09-15 22:19:55 +0200

    Reworked layout to allow for display of multiple IP addresses

--- EV3Splash/src/Splash.java
+++ EV3Splash/src/Splash.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -106,8 +108,10 @@ public class Splash
             (byte) 0xf0, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0x00, 
             (byte) 0x00, (byte) 0x00, (byte) 0xf0, (byte) 0xff, (byte) 0xff, 
             (byte) 0xff, (byte) 0x00, (byte) 0x00, });
-    public static String getIPAddress()
+    
+    public static List<String> getIPAddresses()
     {
+        List<String> result = new ArrayList<String>();
         Enumeration<NetworkInterface> interfaces;
         try
         {
@@ -115,7 +119,7 @@ public class Splash
         } catch (SocketException e1)
         {
             e1.printStackTrace();
-            return "";
+            return null;
         }
         while (interfaces.hasMoreElements()){
             NetworkInterface current = interfaces.nextElement();
@@ -130,21 +134,28 @@ public class Splash
             while (addresses.hasMoreElements()){
                 InetAddress current_addr = addresses.nextElement();
                 if (current_addr.isLoopbackAddress()) continue;
-                return current_addr.getHostAddress();
+                result.add(current_addr.getHostAddress());
             }
         }
-        return "";
+        return result;
     }    
 
     public void displaySplash(String ver)
     {
         g.clear();
         //g.setFont(Font.getDefaultFont());
-        g.drawRegion(logo, 0, 0, logo.getWidth(), logo.getHeight(), 0, SW / 2, SH / 4 , Graphics.HCENTER | Graphics.VCENTER);
+        g.drawRegion(logo, 0, 0, logo.getWidth(), logo.getHeight(), 0, 0, 0, 0);
+        g.setFont(Font.getLargeFont());
+        g.drawString("leJOS", (SW-logo.getWidth())/ 2 + logo.getWidth(), 0, Graphics.TOP|Graphics.HCENTER);
+        g.drawString("EV3", (SW-logo.getWidth())/ 2 + logo.getWidth(), Font.getLargeFont().getHeight(), Graphics.TOP|Graphics.HCENTER);
         g.setFont(Font.getDefaultFont());
-        g.drawString("leJOS/EV3", SW/2, 3*SH/4, Graphics.BASELINE|Graphics.HCENTER);
-        g.drawString(ver, SW/2, (3*SH/4)+Font.getDefaultFont().getHeight(), Graphics.BASELINE|Graphics.HCENTER);
-        g.drawString(getIPAddress(), SW/2, (3*SH/4)+2*Font.getDefaultFont().getHeight(), Graphics.BASELINE|Graphics.HCENTER);
+        g.drawString(ver, SW/2, logo.getHeight()+ 5, Graphics.TOP|Graphics.HCENTER);
+        int cnt = 1;
+        for(String s:getIPAddresses())
+        {
+            g.drawString(s, SW/2, logo.getHeight()+ 5 + cnt*Font.getDefaultFont().getHeight(), Graphics.TOP|Graphics.HCENTER);
+            cnt++;
+        }
         LCD.refresh();
     }
     
