cf8b5f57b3b1a6eaa656ba98785339934ba9b545
lgriffiths <lawrie.griffiths@ntlworld.com>
2013-10-12 14:19:31 +0100

    Support for remote analog ports in RMI server sample

--- EV3RMIServer/src/RemoteEV3.java
+++ EV3RMIServer/src/RemoteEV3.java
@@ -1,9 +1,9 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import lejos.remote.ev3.RMIAnalogPort;
 import lejos.remote.ev3.RMIBattery;
 import lejos.remote.ev3.RMIEV3;
-import lejos.remote.ev3.RMIPort;
 
 
 public class RemoteEV3 extends UnicastRemoteObject implements RMIEV3 {
@@ -15,13 +15,13 @@ public class RemoteEV3 extends UnicastRemoteObject implements RMIEV3 {
 	}
 
 	@Override
-	public RMIPort getPort(String portName) throws RemoteException {
-		return null;
+	public RMIBattery getBattery() throws RemoteException {
+		return new RemoteBattery();
 	}
 
 	@Override
-	public RMIBattery getBattery() throws RemoteException {
-		return new RemoteBattery();
+	public RMIAnalogPort openAnalogPort(String portName) throws RemoteException {
+		return new RemoteAnalogPort(portName);
 	}
 
 }
