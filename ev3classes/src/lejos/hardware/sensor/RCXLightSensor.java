5a45033184103458a5ff228bff0a199e70cb9a03
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2013-11-12 14:12:06 +0000

    Converted RCXLightSensor

--- ev3classes/src/lejos/hardware/sensor/RCXLightSensor.java
+++ ev3classes/src/lejos/hardware/sensor/RCXLightSensor.java
@@ -1,13 +1,8 @@
 package lejos.hardware.sensor;
 
-import lejos.hardware.port.LegacySensorPort;
 import lejos.hardware.port.Port;
 import lejos.robotics.*;
 
-/*
- * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
- * DO NOT EDIT THE VERSION IN pccomms AS IT WILL BE OVERWRITTEN WHEN THE PROJECT IS BUILT.
- */
 
 /**
  *  This class is used to obtain readings from a legacy RCX light sensor, using an adapter cable
@@ -17,12 +12,8 @@ import lejos.robotics.*;
  *  155 and sunlight is around 713.
  * 
  */
-public class RCXLightSensor extends AnalogSensor implements SensorConstants, LampController {
-	
-	private int _zero = 1023;
-	private int _hundred = 0;
-	private static final int SWITCH_DELAY = 10;
-	
+public class RCXLightSensor extends AnalogSensor implements SensorConstants, LampController, SensorMode {
+	private static final int SWITCH_DELAY = 10;	
 	private boolean floodlight = false;
 	
 	/**
@@ -30,47 +21,18 @@ public class RCXLightSensor extends AnalogSensor implements SensorConstants, Lam
 	 * The sensor will be activated, i.e. the LED will be turned on.
 	 * @param port port, e.g. Port.S1
 	 */
-	public RCXLightSensor(Port pt)
+	public RCXLightSensor(Port port)
 	{
-		super(pt);
-		port.setTypeAndMode(TYPE_REFLECTION, MODE_RAW);
-		setFloodlight(true); // Light sensitivity poor when off.
-		
+		super(port);
+		init();	
 	}
 	
-	/**
-	  * Activates an RCX light sensor. This method should be called
-	  * if you want to get accurate values from an RCX
-	  * sensor. In the case of RCX light sensors, you should see
-	  * the LED go on when you call this method.
-	  * @deprecated Use {@link #setFloodlight(boolean)} with true instead 
-	  */
-    @Deprecated
-	public void activate()
-	{
-		setFloodlight(true);
-	}
-	
-	/**
-     * Passivates an RCX light sensor.
-     * @deprecated Use {@link #setFloodlight(boolean)} with false instead 
-	 */
-    @Deprecated
-	public void passivate()
-	{
-		setFloodlight(false);
-	}
-	
-	/**
-	 * Read the current sensor value.
-	 * @return Value as a percentage.
-	 * @deprecated Use {@link #getLightValue()} instead 
-	 */
-    @Deprecated
-	public int readValue()
-	{
-		return getLightValue();
-	}
+    protected void init()
+    {
+        setModes(new SensorMode[]{ this, new AmbientMode() });
+        port.setTypeAndMode(TYPE_REFLECTION, MODE_RAW);
+        setFloodlight(true);
+    }
 
 	public int getFloodlight() {
 		if(this.floodlight == true)
@@ -89,7 +51,6 @@ public class RCXLightSensor extends AnalogSensor implements SensorConstants, Lam
 			switchType(TYPE_REFLECTION, SWITCH_DELAY);
 		else
 	         switchType(TYPE_CUSTOM, SWITCH_DELAY);
-;
 	}
 
 	public boolean setFloodlight(int color) {
@@ -102,41 +63,51 @@ public class RCXLightSensor extends AnalogSensor implements SensorConstants, Lam
 		} else return false;
 	}
 	
-	public int getLightValue() {
-		return ((1023 - NXTRawIntValue(port.getPin1())) * 100/ 1023); 
-	}
-
-	public int getNormalizedLightValue() {
-		return 1023 - NXTRawIntValue(port.getPin1());
+	public SensorMode getRedMode()
+	{
+	    return this;
 	}
 	
-	/**
-	 * call this method when the light sensor is reading the low value - used by readValue
-	 **/
-		public void calibrateLow()
-		{
-			_zero = NXTRawIntValue(port.getPin1());
-		}
-	/** 
-	 *call this method when the light sensor is reading the high value - used by readValue
-	 */	
-		public void calibrateHigh()
-		{
-			_hundred = NXTRawIntValue(port.getPin1());
-		}
-		/** 
-		 * set the normalized value corresponding to readValue() = 0
-		 * @param low the low value
-		 */
-		public void setLow(int low) { _zero = 1023 - low;}
-		  /** 
-	     * set the normalized value corresponding to  readValue() = 100;
-	     * @param high the high value
-	     */
-	    public void setHigh(int high) { _hundred = 1023 - high;}
-	   
-	    public int getLow() { return 1023 - _zero;}
-	    
-	    public int  getHigh() {return 1023 - _hundred;}
+	public SensorMode getAmbientMode()
+	{
+	    return getMode(1);
+	}
+	
+	@Override
+	public int sampleSize() {
+		return 1;
+	}
 
+	@Override
+	public void fetchSample(float[] sample, int offset) {
+        setFloodlight(true);
+        sample[offset] = 1.0f - normalize(port.getPin1());	
+	}
+
+	@Override
+	public String getName() {
+		return "Red";
+	}
+		
+	private class AmbientMode implements SensorMode
+	{
+        @Override
+        public int sampleSize()
+        {
+            return 1;
+        }
+
+        @Override
+        public void fetchSample(float[] sample, int offset)
+        {
+            setFloodlight(false);
+            sample[offset] = 1.0f - normalize(port.getPin1());
+        }
+
+        @Override
+        public String getName()
+        {
+            return "None";
+        }    
+	}
 }
