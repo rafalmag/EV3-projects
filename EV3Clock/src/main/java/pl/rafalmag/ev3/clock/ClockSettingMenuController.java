2209a740d08888e7a8807dec24d6c4ebad8bc8ca
Rafal Magda <rafalmag@gmail.com>
2014-09-17 00:01:18 +0200

    added "holding a button" feature while adjusting clock hand settings

--- EV3Clock/src/main/java/pl/rafalmag/ev3/clock/ClockSettingMenuController.java
+++ EV3Clock/src/main/java/pl/rafalmag/ev3/clock/ClockSettingMenuController.java
@@ -1,8 +1,16 @@
 package pl.rafalmag.ev3.clock;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.Font;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.lcd.TextLCD;
 import lejos.utility.TextMenu;
-import pl.rafalmag.ev3.Time;
+import pl.rafalmag.ev3.Ev3Utils;
+import pl.rafalmag.ev3.RuntimeInterruptedException;
 
 public class ClockSettingMenuController {
 	private final AnalogClock clock;
@@ -12,39 +20,86 @@ public class ClockSettingMenuController {
 	}
 
 	public void clockSettingMenu() {
-		Time time = clock.getTime();
 		LCD.drawString("Clock setting", 0, 0);
 		TextMenu textMenu = new TextMenu(ClockSettingMenu.getNames(), 2);
 		int lastSelected = 0;
 		while (true) {
-			textMenu.setTitle(time.toString());
+			textMenu.setTitle(clock.getTime().toString());
 			// blocking here
 			lastSelected = textMenu.select(lastSelected);
 			if (lastSelected < 0) { // esc
-				clock.setTime(time);
 				return;
 			}
 			ClockSettingMenu clockSettingMenu = ClockSettingMenu.values()[lastSelected];
 			switch (clockSettingMenu) {
 			case HOUR_MINUS:
-				time = time.minusHour();
+				updateClockWhileEnterIsDown(new Runnable() {
+
+					@Override
+					public void run() {
+						clock.setTime(clock.getTime().minusHour());
+					}
+				});
 				break;
 			case HOUR_PLUS:
-				time = time.plusHour();
+				updateClockWhileEnterIsDown(new Runnable() {
+
+					@Override
+					public void run() {
+						clock.setTime(clock.getTime().plusHour());
+					}
+				});
 				break;
 			case MINUTE_MINUS:
-				time = time.minusMinute();
+				updateClockWhileEnterIsDown(new Runnable() {
+
+					@Override
+					public void run() {
+						clock.setTime(clock.getTime().minusMinute());
+					}
+				});
 				break;
 			case MINUTE_PLUS:
-				time = time.plusMinute();
+				updateClockWhileEnterIsDown(new Runnable() {
+
+					@Override
+					public void run() {
+						clock.setTime(clock.getTime().plusMinute());
+					}
+				});
 				break;
 			case OK:
-				clock.setTime(time);
 				return;
 			default:
 				throw new IllegalStateException("Not supported enum value = "
 						+ clockSettingMenu);
 			}
 		}
+	}
+
+	private void updateClockWhileEnterIsDown(final Runnable action) {
+		LCD.clear();
+		final TextLCD timeLCD = BrickFinder.getDefault().getTextLCD(
+				Font.getLargeFont());
+		final ScheduledExecutorService executor = Executors
+				.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				action.run();
+				timeLCD.clear();
+				Ev3Utils.displayLargeText(timeLCD, clock.getTime().toString());
+
+			}
+		}, 0, 500, TimeUnit.MILLISECONDS);
+		Ev3Utils.waitTillEnterIsDown();
+		executor.shutdown();
+		try {
+			executor.awaitTermination(1, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			throw new RuntimeInterruptedException(e);
+		}
+		timeLCD.clear();
 	}
 }
